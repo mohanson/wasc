3bc9c71e0c83c2bbb768b1d88d5a77ad6b3f91a7
mohanson <mohanson@outlook.com>
2020-07-02 20:10:27 +0800

    Submit for stash 09

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,39 @@ use wasc::context;
 use wasc::dummy;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    rog::reg("wasc");
-    rog::reg("wasc::aot_generator");
-    rog::reg("wasc::code_builder");
-    rog::reg("wasc::compile");
-
-    let mut source = String::from("");
-    let mut platform = String::from("");
-    let mut wavm = String::from("wavm");
+    let mut fl_source = String::from("");
+    let mut fl_platform = String::from("");
+    let mut fl_wavm = String::from("wavm");
+    let mut fl_verbose = false;
     {
         let mut ap = argparse::ArgumentParser::new();
         ap.set_description("WASC: WebAssembly native compilter");
-        ap.refer(&mut source)
+        ap.refer(&mut fl_source)
             .add_argument("source", argparse::Store, "WASM/WA(S)T source file");
-        ap.refer(&mut platform).add_option(
+        ap.refer(&mut fl_platform).add_option(
             &["-p", "--platform"],
             argparse::Store,
             "posix_x86_64 posix_x86_64_spectest posix_x86_64_wasi",
         );
-        ap.refer(&mut wavm)
+        ap.refer(&mut fl_wavm)
             .add_option(&["--wavm"], argparse::Store, "WAVM binary");
+        ap.refer(&mut fl_verbose)
+            .add_option(&["-v", "--verbose"], argparse::StoreTrue, "");
         ap.parse_args_or_exit();
     }
-    if source.is_empty() {
+    if fl_source.is_empty() {
         rog::println!("wasc: missing file operand");
         std::process::exit(1);
     }
+    if fl_verbose {
+        rog::reg("wasc");
+        rog::reg("wasc::aot_generator");
+        rog::reg("wasc::code_builder");
+        rog::reg("wasc::compile");
+    }
 
     let mut config = context::Config::default();
-    config.platform = match platform.as_str() {
+    config.platform = match fl_platform.as_str() {
         "posix_x86_64" => context::Platform::PosixX8664,
         "posix_x86_64_spectest" => context::Platform::PosixX8664Spectest,
         "posix_x86_64_wasi" => context::Platform::PosixX8664Wasi,
@@ -49,9 +53,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             std::process::exit(1);
         }
     };
-    config.binary_wavm = wavm;
+    config.binary_wavm = fl_wavm;
 
-    let mut middle = compile::compile(&source, config)?;
+    let mut middle = compile::compile(&fl_source, config)?;
     aot_generator::generate(&mut middle)?;
 
     let mut ep_file = code_builder::CodeBuilder::place(&middle.path_c);
