a63ded0ed3b92703c999fcd39ca10ff5ec83d06a
mohanson <mohanson@outlook.com>
2020-07-08 12:29:03 +0800

    Submit for stash 00

--- third_party/build.sh
+++ third_party/build.sh
@@ -17,3 +17,13 @@ if [ ! -d "wasi-sdk-11.0" ]; then
     ln -s wasi-sdk-11.0 wasi-sdk
     rm wasi-sdk-11.0-linux.tar.gz
 fi
+
+if [ ! -d "riscv-gnu-toolchain" ]; then
+    sudo apt-get install -y autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev
+    git clone --recursive https://github.com/nervosnetwork/ckb-riscv-gnu-toolchain
+    cd ckb-riscv-gnu-toolchain
+    git checkout 528f06df2515352a7d395d0329a494d097ed1181
+    git submodule update --init --recursive
+    ./configure --prefix=./build --with-arch=rv64imc
+    make linux
+fi
