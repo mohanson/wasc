81824fe56ec9201636f900f6a725bce6ad8ff0fb
mohanson <mohanson@outlook.com>
2020-07-13 20:17:23 +0800

    Submit for stash 03

--- src/platform/ckb_vm_assemblyscript.h
+++ src/platform/ckb_vm_assemblyscript.h
@@ -55,15 +55,39 @@ void invalidFloatOperationTrap()
     exit(1);
 }
 
-uint64_t __atomic_load_8(void* p, int32_t _mode)
+uint64_t __atomic_load_8(void *p, int32_t _mode)
 {
-  (void) _mode;
-  return *((uint64_t*) ((uintptr_t) p));
+    (void)_mode;
+    return *((uint64_t *)((uintptr_t)p));
 }
 
-void* wavm_env_abort(void *dummy, int32_t a, int32_t b, int32_t c, int32_t d)
+void *wavm_env_abort(void *dummy, int32_t a, int32_t b, int32_t c, int32_t d)
 {
     exit(1);
+}
+
+static inline long __internal_syscall(long n, long _a0, long _a1, long _a2,
+                                      long _a3, long _a4, long _a5)
+{
+    register long a0 asm("a0") = _a0;
+    register long a1 asm("a1") = _a1;
+    register long a2 asm("a2") = _a2;
+    register long a3 asm("a3") = _a3;
+    register long a4 asm("a4") = _a4;
+    register long a5 asm("a5") = _a5;
+    register long syscall_id asm("a7") = n;
+    asm volatile("scall"
+                 : "+r"(a0)
+                 : "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(syscall_id));
+    return a0;
+}
+
+wavm_ret_int64_t wavm_env_syscall(void *dummy, int64_t n, int64_t _a0, int64_t _a1, int64_t _a2, int64_t _a3, int64_t _a4, int64_t _a5)
+{
+    wavm_ret_int64_t ret;
+    ret.dummy = dummy;
+    ret.value = __internal_syscall(n, _a0, _a1, _a2, _a3, _a4, _a5);
+    return ret;
 }
 
 #endif /* WAVM_CKB_VM_ASSEMBLYSCRIPT_H */
